3f45416ec0112242c641025601954c27a0873a96
Brian Rinaldi <brian.rinaldi@gmail.com>
2024-08-05 13:22:21 -0400

    Remove node modules and cdk out

--- cdk.out/asset.fffa7d1d8eb11eb3ae1f6b0369c0476b496d2c55302ccf9a94469fa6b49d110a/index.mjs
+++
@@ -1,9 +0,0 @@
-export const handler = async (event) => {
-  // perform the operation
-  // return a response once it is completed
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
-};
